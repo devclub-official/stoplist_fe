f5d6a5469454ccbcd5ae448f133b77fd06a885ca
choihooo <hochoi8621@naver.com>
2025-04-19 00:27:16 +0900

    기본 설정

---
+++ src/api/apiClient.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: 'https://localhost:8080', 
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
